ffd2425335a947806c65f097b9ebe4633b710127
tOWERoFjOY <dandrus1617@gmail.com>
2023-09-05 16:16:20 -0400

    Local configurations

--- tableau-transfer/Tableau-Append.py
+++ tableau-transfer/Tableau-Append.py
@@ -15,6 +15,7 @@ dev = "YES"
 
 if dev == "YES":
     from dotenv import load_dotenv
+
     load_dotenv("./tableau-transfer/.env")
 
 # Env Variables
@@ -46,6 +47,8 @@ server = os.getenv('server', 'tableau')
 # Load Tableau Credentials
 tableau_username = os.getenv("ts_username")
 tableau_password = os.getenv("ts_password")
+tableau_TOKEN_name = os.getenv("TS_TOKEN_NAME")
+tableau_TOKEN = os.getenv("TS_TOKEN")
 
 # Load Datawarehouse Credentials
 wh_host = os.getenv("wh_host")
@@ -54,17 +57,22 @@ wh_un = os.getenv("wh_user")
 wh_pw = os.getenv("wh_pass")
 
 # Build Connection & Query Warehouse
-connection_url = URL.create(
-                    "mssql+pyodbc",
-                    username=wh_un,
-                    password=wh_pw,
-                    host=wh_host,
-                    database=wh_db,
-                    query={
-                        "driver": "ODBC Driver 17 for SQL Server"
-                    },
-                )
-engine = sa.create_engine(connection_url)
+if dev == "NO":
+    connection_url = URL.create(
+        "mssql+pyodbc",
+        username=wh_un,
+        password=wh_pw,
+        host=wh_host,
+        database=wh_db,
+        query={
+            "driver": "ODBC Driver 17 for SQL Server",
+        },
+    )
+
+    engine = sa.create_engine(connection_url)
+else:
+    engine = sa.create_engine(
+        "mssql+pyodbc://{}/{}?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server".format(wh_host, wh_db))
 
 # Pull Date and DateTime Columns
 date_cols = """SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
@@ -82,5 +90,26 @@ cols = pd.read_sql_query(int_cols, engine)
 intcols = cols["COLUMN_NAME"].values.tolist()
 
 # Read and write table to hyper file
-print('Extracting Data to Hyper file.', file=sys.stderr)
-count = 0
+
+# Connect to Tableau Server
+server = TSC.Server('https://{}.alleghenycounty.us'.format(server))
+server.version = '3.3'
+if dev == "NO":
+    tableau_auth = TSC.TableauAuth(tableau_username, tableau_password, site_id=site)
+
+    with server.auth.sign_in(tableau_auth):
+        req_option = TSC.RequestOptions()
+        req_option.filter.add(TSC.Filter(TSC.RequestOptions.Field.Name, TSC.RequestOptions.Operator.Equals, name))
+        req_option.filter.add(
+            TSC.Filter(TSC.RequestOptions.Field.ProjectName, TSC.RequestOptions.Operator.Equals, project_name))
+        all_datasources = server.datasources.get(req_option)
+else:
+    tableau_auth = TSC.PersonalAccessTokenAuth(tableau_TOKEN_name,
+                                               tableau_TOKEN, site_id=site)
+
+    with server.auth.sign_in(tableau_auth):
+        req_option = TSC.RequestOptions()
+        req_option.filter.add(TSC.Filter(TSC.RequestOptions.Field.Name, TSC.RequestOptions.Operator.Equals, name))
+        req_option.filter.add(
+            TSC.Filter(TSC.RequestOptions.Field.ProjectName, TSC.RequestOptions.Operator.Equals, project_name))
+        all_datasources = server.datasources.get(req_option)
