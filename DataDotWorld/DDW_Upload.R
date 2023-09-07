2923cbf28104c200e893c144aafaee40b053af6c
tOWERoFjOY <dandrus1617@gmail.com>
2023-09-07 11:07:45 -0400

    DDW_Upload

--- DataDotWorld/DDW_Upload.R
+++ DataDotWorld/DDW_Upload.R
@@ -2,6 +2,7 @@ require(tidyverse, quietly = T)
 require(jsonlite)
 require(httr)
 require(DBI)
+require(writexl)
 
 dev <- Sys.getenv('DEV', "no")# Change to yes when testing locally
 
@@ -12,6 +13,7 @@ if (dev == "yes") {
 dept <- Sys.getenv("DEPT")
 source <- Sys.getenv('SOURCE')
 table <- Sys.getenv("TABLE")
+schema <- Sys.getenv("SCHEMA", "Master")
 
 wh_host <- Sys.getenv('WH_HOST')
 wh_db <- Sys.getenv('WH_DB')
@@ -21,6 +23,23 @@ wh_pass <- Sys.getenv('WH_PASS')
 ddw_org <- Sys.getenv("DDW_ORG", "alleghenycounty")
 ddw_id <- Sys.getenv("DDW_ID", "alco-metadata-reporting")
 Auth_Token <- Sys.getenv('DW_AUTH_TOKEN')
-Upload_Type <- Sys.getenv("PATCH_or_POST","PATCH")
+Project_Filename <- Sys.getenv('PROJECT_NAME')
 
-# Load Data from SQL Server
+# Connect to DataWarehouse
+wh_con <- dbConnect(odbc::odbc(), driver = "{ODBC Driver 17 for SQL Server}", 
+                    server = wh_host,
+                    database = wh_db, 
+                    UID = wh_user, 
+                    pwd = wh_pass)
+
+# Load Table
+table_name <- paste(dept, source, table, sep = "_")
+New_Data <- dbReadTable(wh_con,SQL(paste(schema,table_name,sep = ".")))
+write_xlsx(New_Data, Project_Filename, format_headers = TRUE)
+
+# Upload to DDW Project
+upload_url <- URLencode(paste0("https://api.data.world/v0/uploads/",ddw_org,"/",ddw_id,"/files"))
+r <- VERB("POST",upload_url, body = list(file = upload_file(Project_Filename)),
+          add_headers('Authorization' = paste('Bearer',Auth_Token)),
+          content_type("multipart/form-data"),
+          accept("application/json"))
