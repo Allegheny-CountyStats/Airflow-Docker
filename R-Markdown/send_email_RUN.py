4e9e97ba554356b0381e31418e86a378163ff8be
tOWERoFjOY <dandrus1617@gmail.com>
2023-09-22 10:58:06 -0400

    Docker push 1.0

--- R-Markdown/send_email_RUN.py
+++ R-Markdown/send_email_RUN.py
@@ -6,7 +6,12 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 import os
 
-# specify image attachment with % within html/message
+e_to_emails = os.getenv('TO_EMAILS')
+e_subject = os.getenv('SUBJECT')
+e_message = os.getenv('MESSAGE')
+e_attachment = os.getenv('ATTACHMENT_NAME', None)  # specify image attachment with % within html/message
+e_mailrelay = os.getenv('MAILRELAY')
+
 
 def send_email(subject, from_email, to_emails, message, mailrelay, attachment=None):
     s = smtplib.SMTP(host=mailrelay, port=25)
@@ -14,6 +19,7 @@ def send_email(subject, from_email, to_emails, message, mailrelay, attachment=No
     msg['From'] = from_email
     msg['To'] = to_emails
     msg['Subject'] = subject
+    # Attachment
     if attachment is not None:
         msg.attach(MIMEText(message % attachment, 'html'))
         with open(attachment, 'rb') as fp:
@@ -24,3 +30,6 @@ def send_email(subject, from_email, to_emails, message, mailrelay, attachment=No
     s.send_message(msg)
     del msg
     s.quit()
+
+
+send_email(e_subject, e_to_emails, e_message, e_mailrelay, e_attachment)
