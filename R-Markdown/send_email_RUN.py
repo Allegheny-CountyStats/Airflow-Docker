f7ab1d8d7f7e9e53bc640e84fd4df06999decae1
tOWERoFjOY <dandrus1617@gmail.com>
2023-09-27 11:52:50 -0400

    Update send_email_RUN.py

--- R-Markdown/send_email_RUN.py
+++ R-Markdown/send_email_RUN.py
@@ -10,22 +10,44 @@ e_to_emails = os.getenv('TO_EMAILS')
 e_from_email = os.getenv('FROM_EMAIL')
 e_subject = os.getenv('SUBJECT')
 e_message = os.getenv('MESSAGE')
-e_attachment = os.getenv('ATTACHMENT_NAME', None)  # specify image attachment with % within html/message
+e_img_attachment = os.getenv('ATTACHMENT_NAME', None)  # specify image attachment with % within html/message
+e_file_attachment = os.getenv('FILE_ATTACHMENT', None)
 e_mailrelay = os.getenv('MAILRELAY')
 
 
-def send_email(subject, from_email, to_emails, message, mailrelay, attachment=None):
+def attach_file(filename, msg_mime):
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+    # Add attachment to message and convert message to string
+    msg_mime.attach(part)
+
+
+def send_email(subject, from_email, to_emails, message, mailrelay, file_attach=None, img_attachment=None):
     s = smtplib.SMTP(host=mailrelay, port=25)
     msg = MIMEMultipart()
     msg['From'] = from_email
     msg['To'] = to_emails
     msg['Subject'] = subject
-    # Attachment
-    if attachment is not None:
-        msg.attach(MIMEText(message % attachment, 'html'))
-        with open(attachment, 'rb') as fp:
+    if file_attach is not None:
+        if isinstance(file_attach, list):
+            for f_attach in file_attach:
+                attach_file(f_attach, msg_mime=msg)
+        else:
+            attach_file(file_attach, msg_mime=msg)
+    if img_attachment is not None:
+        msg.attach(MIMEText(message % img_attachment, 'html')) # used for in line insert
+        with open(img_attachment, 'rb') as fp:
             img = MIMEImage(fp.read())
-        img.add_header('Content-ID', '<{}>'.format(attachment))
+        img.add_header('Content-ID', '<{}>'.format(img_attachment))
         msg.attach(img)
     # Send Email Message
     s.send_message(msg)
@@ -34,4 +56,4 @@ def send_email(subject, from_email, to_emails, message, mailrelay, attachment=No
 
 
 send_email(subject=e_subject, from_email=e_from_email, to_emails=e_to_emails, message=e_message,
-           mailrelay=e_mailrelay, attachment=e_attachment)
+           mailrelay=e_mailrelay, file_attach=e_file_attachment, img_attachment=e_img_attachment)
