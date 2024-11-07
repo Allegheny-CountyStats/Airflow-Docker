272aee8e9cc1d1285c878b02279cf142fcd7d46a
geoffreylarnold <geoffrey.arnold@alleghenycounty.us>
2024-11-07 10:10:17 -0500

    Add different schema option

--- inter-warehouse-transfer/inter_warehouse_transfer.R
+++ inter-warehouse-transfer/inter_warehouse_transfer.R
@@ -10,6 +10,7 @@ source <- Sys.getenv("SOURCE")
 dept <- Sys.getenv("DEPT")
 tables <- Sys.getenv('TABLES')
 schema <- Sys.getenv('SCHEMA', 'Reporting')
+schema_b <- Sys.getenv('SCHEMA_B', schema)
 append <- Sys.getenv('APPEND', 'FALSE')
 append <- append == "TRUE"
 
@@ -36,11 +37,12 @@ tables <- unlist(strsplit(tables, ","))
 # Transfer each Table from CJ (Criminal Justice Data Warehouse) to Dave (Data Warehouse Prime)
 for (table in tables) {
   table_name <- paste(schema, paste(dept, source, table, sep = "_"), sep = ".")
+  new_table <- paste(schema_b, paste(dept, source, table, sep = "_"), sep = ".")
   
   temp <- dbReadTable(wha_con, SQL(table_name))
   
   if (max_cols_load == "") { 
-    dbWriteTable(whb_con, SQL(table_name), temp, overwrite = TRUE)
+    dbWriteTable(whb_con, SQL(new_table), temp, overwrite = TRUE)
   } else {
     if (any(max_cols %in% colnames(temp))) {
       # ID Max Cols for this table
@@ -56,9 +58,9 @@ for (table in tables) {
       temp <- select(temp, c(-all_of(cols), everything()))
       
       # Transfer Data to Warehouse
-      dbWriteTable(whb_con, SQL(table_name), temp, field.type = types, overwrite = TRUE)
+      dbWriteTable(whb_con, SQL(new_table), temp, field.type = types, overwrite = TRUE)
     } else {
-      dbWriteTable(whb_con, SQL(table_name), temp, overwrite = TRUE)
+      dbWriteTable(whb_con, SQL(new_table), temp, overwrite = TRUE)
     }
   }
   print(paste(table, "transferred"))
