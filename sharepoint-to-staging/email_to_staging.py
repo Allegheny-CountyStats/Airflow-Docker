d0514cc814b302b7dd5dd9e383f314b3c2723151
tOWERoFjOY <dandrus1617@gmail.com>
2025-04-28 14:50:33 -0400

    First Completed Draft: email to staging

--- sharepoint-to-staging/email_to_staging.py
+++ sharepoint-to-staging/email_to_staging.py
@@ -12,7 +12,8 @@ import sqlalchemy as sa
 import time
 from sqlalchemy.engine import URL
 import re
-from msgraph import GraphServiceClient
+import json
+from io import BytesIO
 
 # Load Datawarehouse Credentials
 wh_host = os.getenv("wh_host")
@@ -44,20 +45,20 @@ client_secret = os.getenv("client_secret")
 
 # Save location
 dept = os.getenv('dept')
-source = os.getenv('source', 'Sharepoint')
+source = os.getenv('source', 'Email')
 table = os.getenv('table')
 table_name = '{}_{}_{}'.format(dept, source, table)
 
-# Information
-drive = os.getenv('drive_id')
-file_id = os.getenv('file_id')
-drive_type = os.getenv('drive_type', 'drives')
+# Email Information
+mailbox_id = os.getenv('mail_id', None)
+message_filter = os.getenv('message_filter')
 
+# Auth creds
 auth = HTTPBasicAuth(client_id, client_secret)
 client = BackendApplicationClient(client_id=client_id)
 oauth = OAuth2Session(client=client)
 
-# Auth
+# Auth token
 token = oauth.fetch_token(
     token_url='https://login.microsoftonline.com/e0273d12-e4cb-4eb1-9f70-8bba16fb968d/oauth2/v2.0/token',
     scope='https://graph.microsoft.com/.default',
@@ -66,16 +67,84 @@ token = oauth.fetch_token(
 bearer = "Bearer {}".format(token['access_token'])
 headers = {'authorization': bearer}
 
-url = "https://graph.microsoft.com/v1.0/users/CountyStat@alleghenycounty.us/mailFolders/inbox"
 
-response = requests.request("GET", url, data="", headers=headers)
-inbox = response.json()
+# Get Request Function
+def get_request(url_name, headers_list):
+    try:
+        response = requests.request("GET", url_name, data="", headers=headers_list)
+        response.raise_for_status()  # Raises HTTPError for bad status codes (4xx or 5xx)
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    else:
+        print("Request was successful.")
+    return response.json()
 
-url = "https://graph.microsoft.com/v1.0/users/CountyStat@alleghenycounty.us/mailFolders/{}/messages?filter=from/emailAddress/address eq 'charles.perry@alleghenycounty.us' and hasAttachments eq true".format(inbox.get("id"))
 
-response = requests.request("GET", url, data="", headers=headers)
-messages = response.json()
+# Inbox
+if mailbox_id is None:
+    url = "https://graph.microsoft.com/v1.0/users/CountyStat@alleghenycounty.us/mailFolders/inbox"
+    inbox = get_request(url, headers)
+    inbox_name = inbox.get("id")
 
-print("hello")
+else:
+    inbox_name = mailbox_id
+
+# Message
+url = "https://graph.microsoft.com/v1.0/users/CountyStat@alleghenycounty.us/mailFolders/{}/messages?filter={}".format(
+        inbox_name, message_filter)
+messages = get_request(url, headers)
+
+att_url = "https://graph.microsoft.com/v1.0/users/CountyStat@alleghenycounty.us/mailFolders/{}/messages/{}/attachments".format(inbox_name, messages.get("value").__getitem__(0).get("id"))
+attachment = get_request(att_url, headers)
+
+att_raw_url = "{}/{}/$value".format(att_url, attachment.get("value").__getitem__(0).get("id"))
+attachment_raw = requests.request("GET", att_raw_url, data="", headers=headers)
+
+if filetype == 'csv':
+    csv_file = BytesIO(attachment_raw.content)
+    df = pd.read_csv(csv_file)
+
+if filetype != 'csv':
+    if filetype == 'x':
+        filename = 'test.xlsx'
+    else:
+        filename = 'test.xls'
+    with open(filename, 'wb') as output:
+        output.write(attachment_raw.content)
+
+    if sheet == '':
+        df = pd.read_excel(filename)
+    elif sheet == 'all':
+        xls = pd.ExcelFile(filename)
+        df = pd.DataFrame()
+        for i in xls.sheet_names:
+            temp = pd.read_excel(filename, sheet_name=i)
+            df = df.append(temp)
+    else:
+        if skip == 0:
+            df = pd.read_excel(filename, sheet_name=sheet)
+        else:
+            df = pd.read_excel(filename, sheet_name=sheet, skiprows=range(0, skip))
+
+    if filetype != 'x':
+        df = df.loc[:, ~df.columns.str.startswith('Unnamed')]
+
+print(df.head())
+
+if snake_case == "YES":
+    def to_snake_case(name):
+        name = re.sub(r"([A-Z]+)([A-Z][a-z])", r"\1_\2", name)
+        name = re.sub(r"([a-z0-9])([A-Z])", r"\1_\2", name)
+        name = re.sub(r"[!@#]", "", name)
+        name = re.sub(r" ", "_", name)
+        name = re.sub(r"^_{,2}", "", name)
+        name = re.sub(r"_{,2}$", "", name)
+        return name.lower()
+
+
+    df.columns = [to_snake_case(col) for col in df.columns]
+
 
 df.to_sql(name=table_name, schema=schema, con=engine, if_exists='replace', index=False)
