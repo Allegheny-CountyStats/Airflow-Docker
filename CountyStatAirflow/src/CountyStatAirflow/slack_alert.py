06f241b49d481841a7b06d4b58fa60f1543c001b
geoffreylarnold <geoffrey.arnold@alleghenycounty.us>
2025-10-15 14:30:27 -0400

    Fix slack alert

--- CountyStatAirflow/src/CountyStatAirflow/slack_alert.py
+++ CountyStatAirflow/src/CountyStatAirflow/slack_alert.py
@@ -9,13 +9,13 @@ def task_fail_slack_alert(context):
             :red_circle: Task Failed. 
             *Task*: {task}  
             *Dag*: {dag} 
-            *Execution Time*: {logical_date}  
+            *Execution Time*: {exec_date}  
             *Log Url*: {log_url} 
             """.format(
         task=context.get('task_instance').task_id,
         dag=context.get('task_instance').dag_id,
         ti=context.get('task_instance'),
-        exec_date=context.get('execution_date'),
+        exec_date=context.get('logical_date'),
         log_url=context.get('task_instance').log_url,
     )
     failed_alert = SlackWebhookOperator(
