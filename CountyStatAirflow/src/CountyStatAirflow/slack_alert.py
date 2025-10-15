c172187ab4cc16f314745e29cb9ae547ebaf0e5f
tOWERoFjOY <dandrus1617@gmail.com>
2025-10-15 15:19:18 -0400

    0.0.7 version

--- CountyStatAirflow/src/CountyStatAirflow/slack_alert.py
+++ CountyStatAirflow/src/CountyStatAirflow/slack_alert.py
@@ -1,26 +1,26 @@
-from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+from airflow.providers.slack.notifications.slack import SlackNotifier
 
 # Slack Alerts
-SLACK_CONN_ID = 'slack'
+SLACK_CONNECTION_ID = "slack"
+SLACK_CHANNEL = "airflow"
 
 
-def task_fail_slack_alert(context):
+def slack_airflow_notification(**context):
     slack_msg = """
-            :red_circle: Task Failed. 
-            *Task*: {task}  
-            *Dag*: {dag} 
-            *Execution Time*: {exec_date}  
-            *Log Url*: {log_url} 
-            """.format(
+                :red_circle: Task Failed. 
+                *Task*: {task}  
+                *Dag*: {dag} 
+                *Execution Time*: {exec_date}  
+                *Log Url*: {log_url} 
+                """.format(
         task=context.get('task_instance').task_id,
         dag=context.get('task_instance').dag_id,
         ti=context.get('task_instance'),
         exec_date=context.get('execution_date'),
         log_url=context.get('task_instance').log_url,
     )
-    failed_alert = SlackWebhookOperator(
-        task_id='slack_alert',
-        slack_webhook_conn_id=SLACK_CONN_ID,
-        message=slack_msg,
-        username='Airflow')
-    return failed_alert.execute(context=context)
+    SlackNotifier(
+        slack_conn_id=SLACK_CONNECTION_ID,
+        text=slack_msg,
+        channel=SLACK_CHANNEL,
+    )
