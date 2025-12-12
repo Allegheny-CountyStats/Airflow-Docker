f83875fd44c65c6f13c8502182ea1cd477b5be07
tOWERoFjOY <dandrus1617@gmail.com>
2025-12-12 12:33:17 -0500

    packaged into version 0.0.11

--- CountyStatAirflow/src/CountyStatAirflow/validates.py
+++ CountyStatAirflow/src/CountyStatAirflow/validates.py
@@ -1,23 +1,26 @@
 from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.sdk import Variable, task_group
+from airflow.models import connection
 
-def validate_task(variable, dept = dept, schema = connection.schema, pool = 'Remote_Pool', url_var = 'docker_remote'):
+
+def validate_task(variable, department_name, schema, connection_name: connection,
+                  pool='Remote_Pool', url_var='docker_remote'):
     validate_schema = DockerOperator(
         task_id=f'Validate_{variable}',
         image='countystats/data-validate:r',
         api_version=Variable.get("docker_api_version"),
         auto_remove='force',
         environment={
-            'DEPT': dept,
+            'DEPT': department_name,
             'TABLE': variable,
             'COL_SCHEMA': Variable.get("""{}_schema""".format(variable)),
             'SOURCE': schema,
-            'WH_HOST': wh_connection.host,
-            'WH_DB': wh_connection.schema,
-            'WH_USER': wh_connection.login,
-            'WH_PASS': wh_connection.password
+            'WH_HOST': connection_name.host,
+            'WH_DB': connection_name.schema,
+            'WH_USER': connection_name.login,
+            'WH_PASS': connection_name.password
         },
-        docker_url= Variable.get(url_var),
+        docker_url=Variable.get(url_var),
         command='Rscript schema-validate.R',
         network_mode="bridge",
         mount_tmp_dir=False,
@@ -25,10 +28,13 @@ def validate_task(variable, dept = dept, schema = connection.schema, pool = 'Rem
     )
     return validate_schema
 
-def data_validation(table_list, ):
+
+@task_group(group_id='data_validations')
+def data_validation(table_list, department_name, schema, connection_name, pool, url_var):
     """
     :rtype: DependencyMixIn
     """
     for tables_l in table_list[0:]:
-        task_iterate = validate_task(tables_l, dept, schema, pool, url_var)
+        task_iterate = validate_task(variable=tables_l, department_name=department_name, schema=schema,
+                                     connection_name=connection_name, pool=pool, url_var=url_var)
         task_iterate
